44cf27525eea30b188e75076dd4c9e7eca0a945c
213899D <213899D@mymail.nyp.edu.sg>
2023-02-05 16:36:54 +0800

    Add project files.

---
+++ WebApplication3/Pages/Login.cshtml.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using WebApplication3.ViewModels;
+
+namespace WebApplication3.Pages
+{
+    public class LoginModel : PageModel
+    {
+		[BindProperty]
+		public Login LModel { get; set; }
+
+		private readonly SignInManager<IdentityUser> signInManager;
+		public LoginModel(SignInManager<IdentityUser> signInManager)
+		{
+			this.signInManager = signInManager;
+		}
+
+		public void OnGet()
+        {
+        }
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (ModelState.IsValid)
+			{
+				var identityResult = await signInManager.PasswordSignInAsync(LModel.Email, LModel.Password,LModel.RememberMe, false);
+
+				if (identityResult.Succeeded)
+				{
+                    return RedirectToPage("Index");
+				}
+
+                ModelState.AddModelError("", "Username or Password incorrect");
+			}
+			return Page();
+		}
+	}
+}
---
+++ WebApplication3/ViewModels/Register.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.ViewModels
+{
+    public class Register
+    {
+
+		[Required]
+		public string FirstName { get; set; }
+
+		[Required]
+		public string LastName { get; set; }
+
+		[Required]
+		public string Gender { get; set; }
+
+		[Required]
+		public string NRIC { get; set; }
+
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+
+		[Required, MinLength(12, ErrorMessage = "Password minimum 12 characters")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Compare(nameof(Password), ErrorMessage = "Password and confirmation password does not match")]
+		public string ConfirmPassword { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		public string DateOfBirth { get; set; }
+
+		[Required]
+		public string Resume { get; set; }
+
+		[Required]
+		public string WhoamI { get; set; }
+	}
+}
